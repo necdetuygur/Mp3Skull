6129a8cf30c0c6bbba032989ca54b797a5907fc3
Necdet UYGUR <necdet.uygur@gmail.com>
2021-04-22 15:34:55 +0300

    AutoDownload

--- site.js
+++ site.js
@@ -2,3 +2,15 @@ setTimeout(function () {
   document.getElementById("input").value = window.location.href.split("#")[1];
   document.getElementById("button").click();
 }, 1e3);
+
+var downloadClicked = false;
+function checkDownload(){
+    if(!downloadClicked){
+        setTimeout(checkDownload, 1e3);
+    }
+    if(document.body.innerText.indexOf("Download") > -1){
+        document.querySelector("#download > a").click();
+        downloadClicked = true;
+    }
+}
+checkDownload();
